d1cd493749b6fe527cf6898fd5487c6dec84db56
sebastian rodriguez <megalobastian12@.com>
2026-08-27 17:53:11 -0500

    Se agrego el metodo dividir

---
+++ Dividir.py
@@ -0,0 +1,9 @@
+def division(num1, num2):
+    division = num1/num2
+    if num2 == 0:
+        print("No se puede divir por 0, intente nuevamente")
+        return
+    else:
+        print("El resultado de la división es: " + str(division))
+        return division
+
