ae8e7af97cf32b509c09ce2a9a00a21f9d6eaeeb
sebastian rodriguez <megalobastian12@.com>
2026-08-27 18:08:02 -0500

    Division

---
+++ Dividir.py
@@ -0,0 +1,8 @@
+def division(num1, num2):
+    division = num1/num2
+    if num2 == 0:
+        print("No se puede divir por 0, intente nuevamente")
+        return
+    else:
+        print("El resultado de la división es: " + division)
+        return division
